dac8f54cd49be5200b19848f7088a0dbc3fba592
nhj123 <ninghengjie@163.com>
2018-08-23 21:30:03 +0800

    添加了一个实体类

--- src/main/java/Df.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by YanYu on 2018/8/23.
- */
-public class Df {
- private int id;
-}
---
+++ src/main/java/com/qf/shop/pojo/Student.java
@@ -0,0 +1,25 @@
+package com.qf.shop.pojo;
+
+/**
+ * Created by YanYu on 2018/8/23.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
